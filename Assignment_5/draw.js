d78a4ba761ccb0b847430ed778e6657e1a9c0561
amadyira25@amherst.edu <amadyira25@amherst.edu>
2023-03-05 14:09:10 -0500

    One line code edited

--- Assignment_5/draw.js
+++ Assignment_5/draw.js
@@ -1,55 +1,54 @@
 function displayPage() {
-    const ns = "http://www.w3.org/2000/svg";
-  
-    //access the svg element
-    const svg = document.querySelector("#canvas");
-  
-    //add a rectangle with a black border
-    let rectangle = document.createElementNS(ns, "rect");
-    rectangle.setAttribute("x", "0");
-    rectangle.setAttribute("y", "0");
-    rectangle.setAttribute("width", "600");
-    rectangle.setAttribute("height", "400");
-    rectangle.setAttribute("stroke", "black");
-    rectangle.setAttribute("stroke-width", "5");
-    rectangle.setAttribute("fill", "white");
-    svg.appendChild(rectangle);
-    //add event listeners to the rectangle
-    let isDrawing = false;
-    let prevX = 0;
-    let prevY = 0;
-    
-    svg.addEventListener("mousedown", (event) => {
-      isDrawing = true;
-      prevX = event.offsetX;
-      prevY = event.offsetY;
-    });
-    
-    svg.addEventListener("mousemove", (event) => {
-      
-      if (isDrawing === true) {
-        // Remove any existing lines
-        while (svg.lastChild && svg.lastChild.tagName === "line") {
-          svg.removeChild(svg.lastChild);
-        }
+  const ns = "http://www.w3.org/2000/svg";
 
-        // Create a new line from the initial mouse down position to the current mouse position
-        let line = document.createElementNS(ns, "line");
-        line.setAttribute("x1", prevX);
-        line.setAttribute("y1", prevY);
-        line.setAttribute("x2", event.offsetX);
-        line.setAttribute("y2", event.offsetY);
-        line.setAttribute("stroke", "black");
-        line.setAttribute("stroke-width", "5");
-    
-        svg.appendChild(line);
+  //access the svg element
+  const svg = document.querySelector("#canvas");
+
+  //add a rectangle with a black border
+  let rectangle = document.createElementNS(ns, "rect");
+  rectangle.setAttribute("x", "0");
+  rectangle.setAttribute("y", "0");
+  rectangle.setAttribute("width", "600");
+  rectangle.setAttribute("height", "400");
+  rectangle.setAttribute("stroke", "black");
+  rectangle.setAttribute("stroke-width", "5");
+  rectangle.setAttribute("fill", "white");
+  svg.appendChild(rectangle);
+  //add event listeners to the rectangle
+  let isDrawing = false;
+  let prevX = 0;
+  let prevY = 0;
+  
+  svg.addEventListener("mousedown", (event) => {
+    isDrawing = true;
+    prevX = event.offsetX;
+    prevY = event.offsetY;
+  });
+  
+  svg.addEventListener("mousemove", (event) => {
+    if (isDrawing === true) {
+      // Remove any existing lines
+      while (svg.lastChild && svg.lastChild.tagName === "line") {
+        svg.removeChild(svg.lastChild);
       }
-    });
-    
-    svg.addEventListener("mouseup", (event) => {
-      isDrawing = false;
-    });
   
-    svg.appendChild(rectangle);
-  }
+      // Create a new line from the initial mouse down position to the current mouse position
+      let line = document.createElementNS(ns, "line");
+      line.setAttribute("x1", prevX);
+      line.setAttribute("y1", prevY);
+      line.setAttribute("x2", event.offsetX);
+      line.setAttribute("y2", event.offsetY);
+      line.setAttribute("stroke", "black");
+      line.setAttribute("stroke-width", "5");
   
+      svg.appendChild(line);
+    }
+  });
+  
+  
+  svg.addEventListener("mouseup", (event) => {
+    isDrawing = false;
+  });
+
+  svg.appendChild(rectangle);
+}
